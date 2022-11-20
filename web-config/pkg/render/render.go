b77257a976f93797658abeaaf2d6da4e4e1ab8de
Yiming Lien <lien6410@gmail.com>
2022-11-20 15:05:25 +0800

    web-config init

---
+++ web-config/pkg/render/render.go
@@ -0,0 +1,79 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func init() {
+	// v1, _ := filepath.Abs("./web-hw")
+	// fmt.Println(v1)
+}
+
+// RenderTemplate renders template using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// create a template cache
+	tc, err := createTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get request template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("template cache not found")
+	}
+
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// render the template
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	// create an empty map[string]*template.Template
+	myCache := map[string]*template.Template{}
+
+	// get all of the files named *.page.html from ./web-config/templates/
+	pages, err := filepath.Glob("./web-config/templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files ending with *.page.html
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./web-config/templates/*.layout.html")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./web-config/templates/*.layout.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
